d1bdf60dc0b7167334df4179a2b340f0817a73ba
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-07-28 14:03:46 +0100

    feat: validation

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import "./formInput.css";
 
 interface Props {
@@ -13,13 +13,23 @@ interface Props {
 }
 
 const FormInput = (props: Props) => {
+  const [focused, setFocused] = useState(false);
   const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
   return (
     <div className="formInput">
       <label>{props.label}</label>
       <input
         {...inputProps}
         onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)}
+        data-focused={focused.toString()}
       />
       <span>{errorMessage}</span>
     </div>
